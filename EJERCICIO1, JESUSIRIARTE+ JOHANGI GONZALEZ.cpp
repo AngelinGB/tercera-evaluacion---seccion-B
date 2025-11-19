45c134ecfffb6f61133e684d86a84b6646b6a4f6
Jesus Iriarte <jesusiriarte08@gmail.com>
2025-11-19 16:27:51 -0400

    Refactor variable names to lowercase and update functions

--- EJERCICIO1, JESUSIRIARTE+ JOHANGI GONZALEZ.cpp
+++ EJERCICIO1, JESUSIRIARTE+ JOHANGI GONZALEZ.cpp
@@ -5,97 +5,96 @@
 using namespace std;
 
 
-#define ARRIBA 72
-#define ABAJO 80
-#define IZQUIERDA 75
-#define DERECHA 77
-#define ESCAPE 27
+#define arriba 72
+#define abajo 80
+#define izquierda 75
+#define derecha 77
+#define escape 27
 
 
-#define ANCHO 30 
-#define ALTO 15 
+#define ancho 30
+#define alto 15
 
 
-
-#define SALIDA_X (ANCHO - 2) 
-#define SALIDA_Y (ALTO - 2) 
+#define salida_x (ancho - 2)
+#define salida_y (alto - 2)
 
 
 void gotoxy(int x, int y) {
-    HANDLE hcon;
-    hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD dwPos;
-    dwPos.X = x;
-    dwPos.Y = y;
-    SetConsoleCursorPosition(hcon, dwPos);
+    HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);
 }
 
 
-void dibujarMuro(int x, int y, int n, char direccion) {
+void dibujarmuro(int x, int y, int n, char direccion) {
     for (int i = 0; i < n; i++) {
         gotoxy(x, y);
-        cout << "#"; 
-        
-        if (direccion == 'H' || direccion == 'h') {
+        cout << "#";
+
+        if (direccion == 'h') {
             x++;
-        } else if (direccion == 'V' || direccion == 'v') {
+        } else if (direccion == 'v') {
             y++;
         }
     }
 }
 
 
-void dibujarLaberinto() {
-
-    dibujarMuro(0, 0, ANCHO, 'H');       
-    dibujarMuro(0, ALTO - 1, ANCHO, 'H');   
-    dibujarMuro(0, 1, ALTO - 2, 'V');     
-    dibujarMuro(ANCHO - 1, 1, ALTO - 2, 'V'); 
+void dibujarlaberinto() {
 
 
-    
+    dibujarmuro(0, 0, ancho, 'h');
+    dibujarmuro(0, alto - 1, ancho, 'h');
+    dibujarmuro(0, 1, alto - 2, 'v');
+    dibujarmuro(ancho - 1, 1, alto - 2, 'v');
 
-    dibujarMuro(5, 5, 15, 'H'); 
-    
+
  
-    dibujarMuro(20, 8, 6, 'V');
+    dibujarmuro(5, 5, 15, 'h');
 
-    gotoxy(SALIDA_X, SALIDA_Y);
-    cout << "X";
+
+    dibujarmuro(20, 8, 6, 'v');
+
+    gotoxy(salida_x, salida_y);
+    cout << "x";
 }
 
-bool verificarColision(int new_x, int new_y) {
-    
-  
-    if (new_x <= 0 || new_x >= ANCHO - 1 || new_y <= 0 || new_y >= ALTO - 1) {
+bool verificarcolision(int new_x, int new_y) {
+
+    if (new_x <= 0 || new_x >= ancho - 1 || new_y <= 0 || new_y >= alto - 1) {
         return true;
     }
 
-  
+
     if (new_y == 5 && new_x >= 5 && new_x <= 19) {
-        return true; 
-    }
-    
-   
-   
-    if (new_x == 20 && new_y >= 8 && new_y <= 13) {
-        return true; 
+        return true;
     }
 
-    return false; 
+
+  
+    if (new_x == 20 && new_y >= 8 && new_y <= 13) {
+        return true;
+    }
+
+    return false;
 }
 
 int main() {
-   
-    int x = 1; 
-    int y = 1; 
+
+    int x = 1;
+    int y = 1;
     char tecla = ' ';
 
-    system("cls"); 
-    dibujarLaberinto(); 
+    system("cls");
+    dibujarlaberinto();
 
-    while (tecla != ESCAPE) {
-      
+    while (tecla != escape) {
+
+    
         gotoxy(x, y);
         cout << "o";
 
@@ -107,39 +106,40 @@ int main() {
                 int next_x = x;
                 int next_y = y;
 
-        
+
                 switch (tecla) {
-                    case ARRIBA:    next_y--; break;
-                    case ABAJO:     next_y++; break;
-                    case IZQUIERDA: next_x--; break;
-                    case DERECHA:   next_x++; break;
+                    case arriba:    next_y--; break;
+                    case abajo:     next_y++; break;
+                    case izquierda: next_x--; break;
+                    case derecha:   next_x++; break;
                 }
 
-                if (!verificarColision(next_x, next_y)) {
-                    
-                  
+                if (!verificarcolision(next_x, next_y)) {
+
+  
                     gotoxy(x, y);
                     cout << " ";
 
-                   
+
+
                     x = next_x;
                     y = next_y;
                 }
 
- 
-                if (x == SALIDA_X && y == SALIDA_Y) {
+
+                if (x == salida_x && y == salida_y) {
                     system("cls");
                     gotoxy(8, 7);
-                    cout << "¡COMPLETADO! LABERINTO SUPERADO.";
-                    getch(); 
-                    tecla = ESCAPE; 
+  
+                    cout << "¡completado! laberinto superado.";
+                    getch();
+                    tecla = escape;
                 }
             }
         }
-        Sleep(10);
+  ;
     }
 
     system("cls");
     return 0;
 }
-
